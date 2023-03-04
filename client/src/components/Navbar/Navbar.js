8cceda65ea0838406eb3d392ae13308a81fb455e
reysachi <86217078+reysachi@users.noreply.github.com>
2023-03-03 19:29:11 -0500

    1. Added Login and Logout Buttons. 2. Moved Navbar from App.js to Navbar.js 3. made navbar more complex

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+/* import statements*/
+import React from 'react';
+import {Link} from 'react-router-dom'; // DO A 'npm install react-router-dom'
+import {AppBar, Typography, Toolbar, Button} from '@material-ui/core';
+import memories from '../../images/memories.png'
+//may or may not be needed
+import useStyles from './styles';
+
+/*
+* navbar from App.js
+*/
+const Navbar = () => {
+    //const classes = useStyles(); -- line not needed
+    const user = null;
+
+    return(
+        <AppBar position="static" color="inherit">
+            <div className={classes.brandContainer}>
+                <Typography component={Link} to="/" variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="dreamio" height= "60" />
+            </div>
+
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>user.result.name.charAt(0)</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>                    
+                    </div>
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color="primary">Login</Button>
+
+                )}
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
