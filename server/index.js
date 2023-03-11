741cbf5acc48e25815a1f86fcabddd3ccd119c0a
mitesh surti <djsurti3003@gmail.com>
2023-03-11 17:16:33 -0500

    Add styling to the client-side

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://djsurt:pwd@cluster0.i7gyzkp.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://djsurt:djsurt123@cluster0.i7gyzkp.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
