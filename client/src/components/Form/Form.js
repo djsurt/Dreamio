ca2a5319d89b8670e0ac7ee4963d00043d653a40
mitesh surti <djsurti3003@gmail.com>
2023-03-17 22:09:41 -0400

    Fix a couple of bugs, now updated posts show up

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,15 +1,15 @@
-import React, {useState} from "react";
+import React, {useState , useEffect} from "react";
 import useStyles from './styles';
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createPost, updatePost } from "../../actions/posts";
 
 //Get the ID of the current post we're on
 
 const Form = ({currentId, setCurrentId}) => {
-    const classes = useStyles();
-
+    
+    //Getting the post 
     const [postData, setPostData] = useState({
         creator:"",
         title:"",
@@ -18,12 +18,19 @@ const Form = ({currentId, setCurrentId}) => {
         selectedFile:""
     });
 
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+    const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        if(post) {setPostData(post)};
+    }, [post]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if(currentId){
+            console.log("Hi");
             dispatch(updatePost(currentId, postData));
         } else{
             dispatch(createPost(postData));
@@ -38,10 +45,10 @@ const Form = ({currentId, setCurrentId}) => {
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
             <Typography variant="h6">Creating a memory</Typography><br/>
-            <TextField name="creator" variant="outlined" label="Creator" fullWidthvalue={postData.creator} onChange={(e) =>  setPostData({...postData, creator: e.target.value})}/>
-            <TextField name="title" variant="outlined" label="Title" fullWidthvalue={postData.title} onChange={(e) =>  setPostData({...postData, title: e.target.value})}/>
-            <TextField name="message" variant="outlined" label="Message" fullWidthvalue={postData.message} onChange={(e) =>  setPostData({...postData, message: e.target.value})}/>
-            <TextField name="tags" variant="outlined" label="Tags" fullWidthvalue={postData.tags} onChange={(e) =>  setPostData({...postData, tags: e.target.value})}/>
+            <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) =>  setPostData({...postData, creator: e.target.value})}/>
+            <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) =>  setPostData({...postData, title: e.target.value})}/>
+            <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) =>  setPostData({...postData, message: e.target.value})}/>
+            <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) =>  setPostData({...postData, tags: e.target.value})}/>
             <div className={classes.fileInput}><FileBase type="file"  multiple = {false}  onDone = {({base64}) => setPostData({...postData, selectedFile:base64})}/></div>
             <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
             <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
