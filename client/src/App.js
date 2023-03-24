94b68595ff65b2a53dcf747c9f64054bf7455e5a
SamRoths <scrothschild45@gmail.com>
2023-03-23 22:39:20 -0400

    implements updating, liking, and deleting posts
    minor changes include creating varibles for the action strings, moving connection_url to a .env file for security, and minor bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ const App = ()=>{
 
     useEffect(() =>{
         dispatch(getPosts());
-    }, [dispatch]);
+    }, [currentId, dispatch]);
     return(
         <Container maxWidth='lg' className={classes.app}>
             <AppBar className={classes.appBar} position="static" color="inherit">
@@ -23,7 +23,7 @@ const App = ()=>{
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid className={classes.mainContainter} container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts setCurrentId={setCurrentId} />
                         </Grid>
--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postsRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -14,9 +15,9 @@ app.use(cors());
 
 app.use('/posts', postsRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://djsurt:djsurt123@cluster0.i7gyzkp.mongodb.net/?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://djsurt:djsurt123@cluster0.i7gyzkp.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4004;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
                 .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
                 .catch((error)=>{console.log(error.message)});
