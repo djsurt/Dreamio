481643a301f8a0680de803281c9a50945152c26c
Isha Das <ixd82@case.edu>
2023-03-26 14:39:50 -0400

    Add dreamio logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -57,8 +57,7 @@ const Navbar = () => {
     return(
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="dreamio" height= "60" />
+                <Link to="/" ><img className={classes.image}  src={memories} alt="dreamio" height= "150" width="150" /></Link>
             </div>
         
         
