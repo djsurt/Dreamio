845981ca25e22fabb479479671de286bc04e0b81
reysachi <86217078+reysachi@users.noreply.github.com>
2023-04-02 17:01:47 -0400

    Fix likes :)

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -17,7 +17,7 @@ export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updated
 
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 
-export const likePost = (id) => axios.patch(`/posts/${id}/likePost`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
 export const signIn = (formData) => API.post('/user/signin', formData);
 export const signUp = (formData) => API.post('/user/signup', formData);
