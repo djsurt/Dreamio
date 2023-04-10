7a6c7b0367c2ca3b0577f5cf0daa3663f2b0d68c
SamRoths <scrothschild45@gmail.com>
2023-04-10 01:24:34 -0400

    pagination and Search
    implements pagination and search features
    
    Note: still requires slight bug fixing of

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,24 +1,40 @@
-import { FETCH_ALL, CREATE, DELETE, UPDATE} from '../constants/actionTypes';
+import {FETCH_BY_SEARCH, FETCH_ALL, START_LOADING, END_LOADING, CREATE, DELETE, UPDATE} from '../constants/actionTypes';
 import * as api from '../api';
 
 //Action Creators: return an action 
 //Since we are using thunk and have to deal with async logic, we add async dispatch
-export const getPosts = ()=> async (dispatch) => {
+export const getPosts = (page)=> async (dispatch) => {
 
     try{
-        const {data} = await api.fetchPosts();
+        dispatch({type: START_LOADING});
+        const {data} = await api.fetchPosts(page);
         //action is an object that has a type and a payload
         dispatch({type: FETCH_ALL, payload: data});
+        dispatch({type: END_LOADING})
     } catch(error){
         console.log(error.message);
     } 
+};
+
+export const getPostsBySearch = (searchQuery) => async (dispatch) => {
+    try{
+        dispatch({type: START_LOADING});
+        const {data: {data}} = await api.fetchPostsBySearch(searchQuery);
+        dispatch({type: FETCH_BY_SEARCH, payload: data});
+        console.log(data);
+        dispatch({type: END_LOADING})
+    } catch(error){
+        console.log(error);
+    }
 }
 
 export const createPost = (post) => async (dispatch) => {
     try{
+        dispatch({type: START_LOADING});
         const { data } = await api.createPost(post); 
 
         dispatch({type: CREATE, payload: data});
+        dispatch({type: END_LOADING})
     } catch(error){
         console.log(error.message);
     }
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,6 @@
 import React, {useState , useEffect} from "react";
 import useStyles from './styles';
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-import TextareaAutosize from '@mui/base/TextareaAutosize'
 import FileBase from 'react-file-base64';
 import { useDispatch, useSelector } from 'react-redux';
 import { createPost, updatePost } from "../../actions/posts";
@@ -31,7 +30,6 @@ const Form = ({currentId, setCurrentId}) => {
         e.preventDefault();
 
         if(currentId){
-            console.log("Hi");
             dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
             
         } else{
@@ -56,24 +54,15 @@ const Form = ({currentId, setCurrentId}) => {
     }
 
     return (
-        <Paper className={classes.paper}>
+        <Paper className={classes.paper} elevation={6}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
             <Typography variant="h6">{currentId? 'Editing':'Creating'} a memory</Typography><br/>
-            <TextField name="title" variant="outlined" label="Type title here..." fullWidth value={postData.title} onChange={(e) =>  setPostData({...postData, title: e.target.value})}/>
-            <TextField name="tags" variant="outlined" label="Enter key tags for this dream..." fullWidth value={postData.tags} onChange={(e) =>  setPostData({...postData, tags: e.target.value.split(',')})}/>
-            {/*<TextField name="message" variant="outlined" label="Have an interesting dream? Share it here..." fullWidth value={postData.message} onChange={(e) =>  setPostData({...postData, message: e.target.value})}/>*/}
-            <TextareaAutosize
-                aria-label = 'message'
-                className ={classes.textareaMessage}
-                minRows={4}
-                placeholder = "What did you dream about today? "
-                value={postData.message} onChange={(e) =>  setPostData({...postData, message: e.target.value})}/>
-            
+            <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) =>  setPostData({...postData, title: e.target.value})}/>
+            <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) =>  setPostData({...postData, message: e.target.value})}/>
+            <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) =>  setPostData({...postData, tags: e.target.value.split(',')})}/>
             <div className={classes.fileInput}><FileBase type="file"  multiple = {false}  onDone = {({base64}) => setPostData({...postData, selectedFile:base64})}/></div>
-            <div className={classes.buttonsTwo}>
-                <Button className={classes.buttonSubmit} variant="contained" size="small" type="submit">Create Memory</Button>
-                <Button className={classes.buttonClear} variant="contained" color="primary" size="small" onClick={clear}>Clear Contents</Button>
-            </div>
+            <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+            <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
             </form>
         </Paper>
     );
