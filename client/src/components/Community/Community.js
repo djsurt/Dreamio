f9093ed9773b93e9821e40ded0494e8884ccd526
mitesh surti <djsurti3003@gmail.com>
2023-04-12 14:55:52 -0400

    Add sentiment Analysis Functionality

--- client/src/components/Community/Community.js
+++ client/src/components/Community/Community.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sentiment from 'sentiment';
 import http from 'http';
 
@@ -16,45 +16,63 @@ const analyzeSentiment = (text) => {
   const normalizedScore = score;
   return normalizedScore;
 }
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-const mongoSentiment = () =>{
-  var value = 0;
-  const req = http.request(options, (res) => {
-    let data = '';
-    res.on('data', (chunk) => {
-      data += chunk;
+const mongoSentiment = async () => {
+  let value = 0;
+  const fetchPosts = async () => {
+    return new Promise((resolve, reject) => {
+      const req = http.request(options, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+      
+        res.on('end', () => {
+          resolve(data);
+        });
+      });
+      req.on('error', (error) => {
+        reject(error);
+      });
+      
+      req.end();
     });
-  
-    res.on('end', () => {
-      let resultingVal = 0;
-      const posts = JSON.parse(data);
-      for(let i = 0; i < posts.length; ++i){
-        resultingVal = resultingVal + (analyzeSentiment(posts[i]['message']));
-      }
-      value = resultingVal;
-    });
-  });
-  req.on('error', (error) => {
+  };
+
+  try {
+    const data = await fetchPosts();
+    let resultingVal = 0;
+    const posts = JSON.parse(data);
+    for (let i = 0; i < posts.length; ++i) {
+      resultingVal = resultingVal + (analyzeSentiment(posts[i]['message']));
+    }
+    value = resultingVal;
+    console.log(value);
+
+    if (value <= -1) {
+      return "The community is feeling down at the moment :(";
+    } else if (value >= 1) {
+      return "The community is happy!!";
+    } else {
+      return "The community is okay👍";
+    }
+  } catch (error) {
     console.error(error);
-  });
-  
-  req.end();
-
-  if(value <= -1){
-    console.log("The community is feeling down at the monment :(");
-  } else if(value >= 1){
-    console.log("The community is happy!!");
-  } else{
-    console.log("The community is okay👍");
   }
-
-}
+};
 
 function Community() {
-  return <div>{mongoSentiment()}</div>;
+  const [sentimentStatus, setSentimentStatus] = useState("");
+
+  useEffect(() => {
+    async function fetchSentimentStatus() {
+      const status = await mongoSentiment();
+      setSentimentStatus(status);
+    }
+    fetchSentimentStatus();
+  }, []);
+
+  return <div>{sentimentStatus}</div>;
 }
 
 export default Community;
