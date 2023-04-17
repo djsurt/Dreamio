3ab2760eddf29032cf81574a68f97d2262554f3a
reysachi <rachelsyamamoto@gmail.com>
2023-04-17 03:05:21 -0400

    added Style to friend reqs

--- client/src/components/UserProfile/UserProfile.js
+++ client/src/components/UserProfile/UserProfile.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { fetchUserByUsername, getUserFriends, getUserFriendRequests, sendFriendRequest, acceptFriendRequest, ignoreFriendRequest } from '../../api';
-
+import useStyles from './styles';//for front end styles
+import {Grid, CircularProgress, Paper, Card, CardHeader, CardContent, Button} from '@material-ui/core';
+//import {CloudIcon, SnoozeIcon, NotificationsIcon, FaceRetouchingOffIcon} from '@mui/icons-material';
+import SnoozeIcon from '@mui/icons-material/Snooze';
+import AddReactionIcon from '@mui/icons-material/AddReaction';
+import CloudIcon from '@mui/icons-material/Cloud';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import FaceRetouchingOffIcon from '@mui/icons-material/FaceRetouchingOff';
+import FaceIcon from '@mui/icons-material/Face';
 const UserProfile = ({ match }) => {
     const { username } = match.params;
+    const classes = useStyles(); //-- line for CSS styles frontend design
     const [userProfile, setUserProfile] = useState(null);
     const [friends, setFriends] = useState([]);
     const [friendRequests, setFriendRequests] = useState([]);
@@ -63,55 +72,92 @@ useEffect(() => {
 
     const isFriend = loggedInUser.result.friends.includes(userProfile._id);
     const hasSentRequest = userProfile.friendRequests.includes(loggedInUser.result._id);
-
+    var profileText = userProfile.name + "'s Profile";
+ 
     return (
-        <div>
-            <h1>{userProfile.name}'s Profile</h1>
-            <p>Email: {userProfile.email}</p>
-            {/* Add more user-specific data here */}
+        <div className={classes.mainContainer}>
+            <Grid className={classes.secondaryContainer} container alignItems="stretch" spacing={3}>
+                <Grid item xs={2}></Grid>
+                <Grid className={classes.center} item xs={8}>
+                    <Card className={classes.cardContainer}>
+                        <CardHeader title={profileText} subheader={userProfile.email}/>
+                        <h1 className={classes.center}>
+                            <CloudIcon fontSize="large"/>&nbsp;&nbsp;
+                            <CloudIcon fontSize="large" color="primary"/>&nbsp;&nbsp;
+                            <CloudIcon fontSize="large"/>&nbsp;&nbsp;
+                            <CloudIcon fontSize="large" color="primary"/>&nbsp;&nbsp;
+                            <CloudIcon fontSize="large"/>
+                        </h1>
+                         <hr className={classes.line}/>
+                        <CardContent className={classes.cardContainer}>
+                           
+                      
+                            {/* <h1>{userProfile.name}'s Profile</h1>
+                            <p>Email: {userProfile.email}</p>*/}
+                  
+                            {loggedInUser.result._id !== userProfile._id && (
+                                isFriend ? (
+                                    <Paper className={classes.paperLength} elevation={4}>
+                                        <p>You are friends.</p>
+                                    </Paper>
+                                ) : hasSentRequest ? (
+                            <div>
+                                <h3>Waiting for response...</h3>
+                                <Button variant="contained" color="secondary" onClick={handleAcceptRequest} startIcon={<AddReactionIcon />}>Accept Friend Request</Button>&nbsp;&nbsp;
+                                <Button variant="contained" color="secondary" onClick={handleIgnoreRequest} startIcon ={<SnoozeIcon />}>Ignore Friend Request</Button>
+                                
+                            </div>
+                            ) : (
+                                <Button variant="contained" color="secondary" onClick={handleSendRequest}>Send Friend Request</Button>
+                            )
+                            )}
 
-            {loggedInUser.result._id !== userProfile._id && (
-                isFriend ? (
-                    <p>You are friends.</p>
-                ) : hasSentRequest ? (
-                    <div>
-                        <button onClick={handleAcceptRequest}>Accept Friend Request</button>
-                        <button onClick={handleIgnoreRequest}>Ignore Friend Request</button>
-                    </div>
-                ) : (
-                    <button onClick={handleSendRequest}>Send Friend Request</button>
-                )
-            )}
-
-            {loggedInUser.result._id === userProfile._id && (
-                <div>
-                    {/* ... friends list */}
-                    
-                    {friends.length === 0 ? (
-                        <p>You have no friends.</p>
-                    ) : (
-                        <ul>
-                            {friends.map((friend) => (
-                                <li key={friend._id}>{friend.name}</li>
-                            ))}
-                        </ul>
+                    {loggedInUser.result._id === userProfile._id && (
+                        <div className={classes.cardContainer}>
+                            <Grid container spacing={3} className ={classes.gridInner}>
+                                <Grid item xs={6} className={classes.cardContainer}>
+                                    <Paper elevation={4} className={classes.paperColor}>
+                                    {/* ... friends list */}
+                                        <h3><FaceIcon /> Friends:</h3>
+                                       
+                                        {friends.length === 0 ? (
+                                           
+                                        <p>  <FaceRetouchingOffIcon fontSize="large"/> You have no friends.</p>
+                                        ) : (
+                                        <ul>
+                                            {friends.map((friend) => (
+                                            <li key={friend._id}>{friend.name}</li>
+                                            ))}
+                                        </ul>
+                                        )}
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs={5} className ={classes.cardContainer}>
+                                    <Paper elevation={4} className={classes.paperColor}>
+                                        <h3><NotificationsIcon color="primary"/> Friend Requests</h3>
+                                    {(
+                                        <ul>
+                                        {friendRequests.map((request) => (
+                                        <li key={request._id}>
+                                            {request.name}
+                                            <Button onClick={() => handleAcceptRequest(request._id)} startIcon={<AddReactionIcon />}>Accept</Button>&nbsp;&nbsp;
+                                            <Button onClick={() => handleIgnoreRequest(request._id)} startIcon={<SnoozeIcon />}>Ignore</Button>
+                                        </li>
+                                        ))}
+                                        </ul>
+                                    )}
+                                    </Paper>
+                                </Grid>
+                            </Grid>
+                        </div>
                     )}
-
-                    <h2>Friend Requests</h2>
-                      {(
-                        <ul>
-                            {friendRequests.map((request) => (
-                                <li key={request._id}>
-                                    {request.name}
-                                    <button onClick={() => handleAcceptRequest(request._id)}>Accept</button>
-                                    <button onClick={() => handleIgnoreRequest(request._id)}>Ignore</button>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </div>
-            )}
-        </div>
+                    </CardContent>
+                    </Card>
+                </Grid>
+        <Grid item xs={2}></Grid>
+            {/* Add more user-specific data here */}       
+    </Grid>
+    </div>
     );
 };
 
---
+++ client/src/components/UserProfile/styles.js
@@ -0,0 +1,100 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { deepPurple } from '@material-ui/core/colors';
+
+export default makeStyles((theme) => ({
+  mainContainer: {
+    borderRadius: 15,
+    width: '90vw',
+    height: '70vh',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  secondaryContainer:{
+    borderRadius: 15,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  cardContainer:{
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+   
+  },
+  /*
+  cardContentContainer:{
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: "yellow",
+  },
+  divFlex:{
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: "pink",
+  },
+  */
+  gridInner:{
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    flexDirection: 'row',
+  },
+  sendFriend:{
+      padding: '8px 20px',
+  },
+  center:{
+      textAlign: 'center',
+  },
+  paperColor:{
+      backgroundColor:"white",
+      display:'flex',
+      flexDirection: "column",
+      flexBasis:'100%',
+  },
+  paperLength:{
+      display:'flex',
+      flexDirection:'row',
+      flexBasis:'80%',
+     
+  },
+  line:{
+      width:'95%',
+      margin:'auto',
+      color: 'rgb(116,113,113)',
+  },
+  /*
+  heading: {
+    color: 'rgba(0,183,255, 1)',
+    textDecoration: 'none',
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '400px',
+  },
+  profile: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '400px',
+  },
+  userName: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  brandContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  */
+  
+}));
