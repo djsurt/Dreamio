b7bc05892914e5c915f95e017f0f3589fd3e9184
mitesh surti <djsurti3003@gmail.com>
2023-04-21 15:50:03 -0400

    Minor fix

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -42,7 +42,7 @@ const Form = ({currentId, setCurrentId}) => {
         return (
             <Paper className={classes.paper}>
                 <Typography variant="h6" align="center">
-                    Please Sign In to create your own memories and like other's memories.
+                    Please Sign In to create your own dreams and like other's dreams.
                 </Typography>
             </Paper>
         );
@@ -56,7 +56,7 @@ const Form = ({currentId, setCurrentId}) => {
     return (
         <Paper className={classes.paper} elevation={6}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-            <Typography variant="h6">{currentId? 'Editing':'Creating'} a memory</Typography><br/>
+            <Typography variant="h6">{currentId? 'Editing':'Creating'} a dream</Typography><br/>
             <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) =>  setPostData({...postData, title: e.target.value})}/>
             <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) =>  setPostData({...postData, message: e.target.value})}/>
             <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) =>  setPostData({...postData, tags: e.target.value.split(',')})}/>
