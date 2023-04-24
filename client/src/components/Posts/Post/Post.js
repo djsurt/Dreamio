c985140d4a476066788eeb2172fa7332fd03b7c0
mitesh surti <djsurti3003@gmail.com>
2023-04-24 16:42:42 -0400

    Make a small change

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -42,12 +42,6 @@ const Post = ({post, setCurrentId}) => {
         </div>
         {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
         <div className={classes.overlay2}>
-          <Button 
-          style={{ color: 'white' }} 
-          size="small" 
-          onClick={() => setCurrentId(post._id)}>
-            <MoreHorizIcon fontSize="default"/>  
-          </Button> 
         </div>
         )}
         <div className={classes.details}>
